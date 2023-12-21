f4703de16498d3b0d4489d9e06e92bbcb8b7e9c3
Versim <wojciechkrzos67@gmail.com>
2023-12-21 16:10:29 +0100

    feat: add file reader

--- day1/day1.1.go
+++ day1/day1.1.go
@@ -3,30 +3,54 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strconv"
 	"strings"
 )
 
 func main() {
-	// Prompt the user for input
-	fmt.Print("Enter a string: ")
-	var input string
-	_, err := fmt.Scanln(&input)
-	if err != nil {
-		fmt.Println("Error reading input:", err)
-		return
-	}
 
-	// Call the function with the user's input
-	sum := calculateSumOfNumbersInString(input)
-	fmt.Println("Sum of numbers in the string:", sum)
+	processedInput := readFile("input.txt")
+
+	stringArr := splitString(processedInput, ' ')
+	sum := sumCreatedNumbersOfStrings(stringArr)
+	fmt.Print(sum)
 }
 
-func calculateSumOfNumbersInString(input string) int {
-	stringArr := splitString(input, ' ')
-	sum := sumCreatedNumbersOfStrings(stringArr)
-	return sum
+func readFile(fileName string) string {
+	// Open the input file
+	file, err := os.Open(fileName)
+
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return "err"
+	}
+	defer file.Close()
+
+	// Create a scanner to read from the file
+	scanner := bufio.NewScanner(file)
+
+	// Create an empty slice to store the lines
+	var lines []string
+
+	// Read lines from the file
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	// Close the scanner
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error reading file:", err)
+		return "err"
+	}
+
+	// Join the lines with spaces and print the result
+	result := strings.Join(lines, " ")
+	fmt.Println(result)
+
+	return result
 }
 
 func sumCreatedNumbersOfStrings(input []string) int {
@@ -84,4 +108,50 @@ func splitString(input string, delimiter rune) []string {
 	elements := strings.Split(input, string(delimiter))
 
 	return elements
+}
+
+// Idea
+
+func createNumberFromFrontAndBack(input []int) int {
+	if len(input) == 0 {
+		return 0
+	}
+
+	// Initialize indices for the front and back of the slice
+	front := 0
+	back := len(input) - 1
+
+	// Initialize variables to store the digits from the front and back
+	frontDigit := input[front]
+	backDigit := input[back]
+
+	// Initialize variables to keep track of the multiplier for the front and back digits
+	frontMultiplier := 1
+	backMultiplier := 1
+
+	// Initialize the result
+	result := 0
+
+	for front <= back {
+		// Add the front digit to the result with the appropriate multiplier
+		result += frontDigit * frontMultiplier
+
+		// If the front and back indices are the same, break out of the loop
+		if front == back {
+			break
+		}
+
+		// Add the back digit to the result with the appropriate multiplier
+		result += backDigit * backMultiplier
+
+		// Update multipliers
+		frontMultiplier *= 10
+		backMultiplier *= 10
+
+		// Move the indices
+		front++
+		back--
+	}
+
+	return result
 }
